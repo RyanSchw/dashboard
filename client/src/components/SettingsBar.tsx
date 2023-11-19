25d726f86ee33885f1469915736f26854c25c447
Ryan Schwartz <32225075+RyanSchw@users.noreply.github.com>
2023-11-18 18:06:08 -0800

    Add outline for settings bar

--- client/src/components/SettingsBar.tsx
+++ client/src/components/SettingsBar.tsx
@@ -1,14 +1,25 @@
 import React, { useEffect, useState } from 'react';
-import { Box, Text } from 'grommet';
+import { Box, Button, Text } from 'grommet';
+import { Down, Moon, Sun, Up } from 'grommet-icons';
 
 function SettingsBar() {
     return (
         <Box gridArea='settings'>
-            <div style={{
-                height: '100%',
-                width: '100%',
-                backgroundColor: 'grey',
-            }} />
+            <Box border align='center'>
+                <Box margin={{ top:'small', bottom: 'xsmall' }}>
+                    <Sun />
+                </Box>
+                <Button icon={<Up />} disabled />
+                <Button icon={<Down />} disabled />
+            </Box>
+
+            <Box margin='xsmall' />
+
+            <Box border align='center'>
+                <Button icon={<Moon />} disabled />
+            </Box>
+
+
         </Box>
     );
 }
